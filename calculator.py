042ce13c61694e3582289b38e1701702ee7c2585
Balasubramani, Saravana Prakash <SBalasubramani@ashleyfurniture.com>
2026-03-22 01:17:52 +0530

    PR testing

--- calculator.py
+++ calculator.py
@@ -4,7 +4,7 @@ def add_two_numbers(a, b):
 
 def main():
     print('5 + 10 =', add_two_numbers(5, 10))
-    print('15.5 + 20.3 =', add_two_numbers(15.5, 20.3))
+    print('15.5 + 20.3 =', add_two_numbers(15.5, 20.3));
 
 if __name__ == '__main__':
     main()
